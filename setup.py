2fe5522102e40953cbbee974d890e0eda7c4f656
proofx <achievement008@gmail.com>
2018-09-13 13:34:20 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='django-parsed-field',
+    version='1.0',
+    packages=['django-parsed-field'],
+    url='https://github.com/achievement008/django-parsed-field',
+    license='MIT',
+    author='proofx',
+    author_email='achievement008@gmail.com',
+    description=open("README.rst").read(),
+)
